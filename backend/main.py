d701093f9103becb971ad446af24ddb393dc372a
LautiLR <lautarolopezrucci@gmail.com>
2026-04-06 21:38:59 -0300

    main.py

--- backend/main.py
+++ backend/main.py
@@ -70,6 +70,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
+    allow_origins=["*"]
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
